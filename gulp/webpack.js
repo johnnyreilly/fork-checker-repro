ff7c1e4a8c9ac80a9dfda225fc99dcd11fa04e47
John Reilly <johnny_reilly@hotmail.com>
2017-05-08 18:58:35 +0100

    add fork checker

--- gulp/webpack.js
+++ gulp/webpack.js
@@ -4,97 +4,120 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
-var failPlugin = require('webpack-fail-plugin');
+var webpackFailPlugin = require('webpack-fail-plugin');
+var ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 var webpackConfig = require('../webpack.config.js');
 var packageJson = require('../package.json');
 
 function buildProduction(done) {
-   // modify some webpack config options
-   var myProdConfig = webpackConfig;
-   myProdConfig.output.filename = '[name].[hash].js';
+  // modify some webpack config options
+  var myProdConfig = webpackConfig;
+  myProdConfig.output.filename = '[name].[hash].js';
 
-   myProdConfig.plugins = myProdConfig.plugins.concat(
-      new webpack.DefinePlugin({
-          'process.env': {
-              'NODE_ENV': JSON.stringify('production')
-         }
-      }),
-      new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.[hash].js' }),
-      new webpack.optimize.UglifyJsPlugin({
-         compress: {
-            warnings: true
-         }
-      }),
-      failPlugin
-   );
+  setToTranspile(myDevConfig);
 
-   // run webpack
-   webpack(myProdConfig, function (err, stats) {
-      if (err) { throw new gutil.PluginError('webpack:build', err); }
-      gutil.log('[webpack:build]', stats.toString({
-         colors: true
-      }));
+  myProdConfig.plugins = myProdConfig.plugins.concat(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.[hash].js' }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      blockEmit: true,
+      tslint: false,
+      watch: ['./src', './test'] // optional but improves performance (less stat calls)
+    }),
+    webpackFailPlugin
+  );
 
-      if (done) { done(); }
-   });
+  // run webpack
+  webpack(myProdConfig, function (err, stats) {
+    if (err) { throw new gutil.PluginError('webpack:build', err); }
+    gutil.log('[webpack:build]', stats.toString({
+      colors: true
+    }));
+
+    if (done) { done(); }
+  });
 }
 
 function createDevCompiler() {
-   // modify some webpack config options
-   var myDevConfig = webpackConfig;
-   myDevConfig.devtool = 'inline-source-map';
+  // modify some webpack config options
+  var myDevConfig = webpackConfig;
+  myDevConfig.devtool = 'inline-source-map';
 
-   myDevConfig.plugins = myDevConfig.plugins.concat(
-      new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' }),
-      new WebpackNotifierPlugin({ title: 'Webpack build', excludeWarnings: true })
-   );
+  setToTranspile(myDevConfig);
 
-   // create a single instance of the compiler to allow caching
-   return webpack(myDevConfig);
+  myDevConfig.plugins = myDevConfig.plugins.concat(
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' }),
+    new WebpackNotifierPlugin({ title: 'Webpack build', excludeWarnings: true }),
+    new ForkTsCheckerWebpackPlugin({
+      blockEmit: false,
+      tslint: false,
+      watch: ['./src', './test'] // optional but improves performance (less stat calls)
+    })
+  );
+
+  // create a single instance of the compiler to allow caching
+  return webpack(myDevConfig);
+}
+
+function setToTranspile(config) {
+  config.module.rules[0].use[1].options = {
+    transpile: true
+  };
+  return config;
 }
 
 function build() {
-   return new Promise(function (resolve, reject) {
-      buildProduction(function (err) {
-         if (err) {
-            reject(err);
-         } else {
-            resolve('webpack built');
-         }
-      });
-   });
+  return new Promise(function (resolve, reject) {
+    buildProduction(function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('webpack built');
+      }
+    });
+  });
 }
 
 function watch() {
-   var firstBuildDone = false;
+  var firstBuildDone = false;
 
-   return new Promise(function (resolve, reject) {
-      var devCompiler = createDevCompiler();
-      devCompiler.watch({ // watch options:
-         aggregateTimeout: 300 // wait so long for more changes
-      }, function (err, stats) {
-         if (err) {
-            if (!firstBuildDone) {
-               firstBuildDone = true;
-               reject(err);
-            }
-            throw new gutil.PluginError('webpack:build-dev', err);
-         } else {
-            if (!firstBuildDone) {
-               firstBuildDone = true;
-               resolve('webpack built');
-            }
-         }
+  return new Promise(function (resolve, reject) {
+    var devCompiler = createDevCompiler();
+    devCompiler.watch({ // watch options:
+      aggregateTimeout: 300 // wait so long for more changes
+    }, function (err, stats) {
+      if (err) {
+        if (!firstBuildDone) {
+          firstBuildDone = true;
+          reject(err);
+        }
+        throw new gutil.PluginError('webpack:build-dev', err);
+      } else {
+        if (!firstBuildDone) {
+          firstBuildDone = true;
+          resolve('webpack built');
+        }
+      }
 
-         gutil.log('[webpack:build-dev]', stats.toString({
-            chunks: false,
-            colors: true
-         }));
-      });
-   });
+      gutil.log('[webpack:build-dev]', stats.toString({
+        chunks: false,
+        colors: true
+      }));
+    });
+  });
 }
 
 module.exports = {
-   build: function () { return build(); },
-   watch: function () { return watch(); }
+  build: function () { return build(); },
+  watch: function () { return watch(); }
 };
--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,16 @@
 'use strict';
 
 var webpackConfig = require('./webpack.config.js');
+var ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = function(config) {
+webpackConfig.module.rules[0].use[1].options = {
+  transpile: true
+};
+
+module.exports = function (config) {
   // Documentation: https://karma-runner.github.io/0.13/config/configuration-file.html
   config.set({
-    browsers: [ 'PhantomJS' ],
+    browsers: ['PhantomJS'],
 
     files: [
       // This ensures we have the es6 shims in place and then loads all the tests
@@ -15,18 +20,24 @@ module.exports = function(config) {
 
     port: 9876,
 
-    frameworks: [ 'jasmine' ],
+    frameworks: ['jasmine'],
 
     logLevel: config.LOG_INFO, //config.LOG_DEBUG
 
     preprocessors: {
-      'test/main.js': [ 'webpack', 'sourcemap' ]
+      'test/main.js': ['webpack', 'sourcemap']
     },
 
     webpack: {
       devtool: 'inline-source-map',
       module: webpackConfig.module,
-      resolve: webpackConfig.resolve
+      resolve: webpackConfig.resolve,
+      plugins: [
+        new ForkTsCheckerWebpackPlugin({
+          tslint: false,
+          watch: ['./src', './test'] // optional but improves performance (less stat calls)
+        })
+      ]
     },
 
     webpackMiddleware: {
